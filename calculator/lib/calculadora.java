16256f134ff8ffe0645f6fd062b98ec8b9729d5e
joze-hub <jose.herandez2021@jupa.superate.org.pa>
2024-02-20 22:40:30 -0500

    Add comments on the calculator project

--- calculator/lib/calculadora.java
+++ calculator/lib/calculadora.java
@@ -13,12 +13,18 @@ public class calculadora implements ActionListener{
     JTextField textField; //Entrada de texto
     JButton [] numeroBotones = new JButton[10]; //Aqui distribuiremos los botones con los números
     JButton [] botonFunciones = new JButton[9]; //Aquí distribuiremos los botones con las funciones
+
+    //Estos son los botones de la calculadora
     JButton addButton,subButton,mulButton,divButton;
     JButton decButton,eqButton,delButton,clrButton, negButton;
     JPanel panel;
 
-    Font myFont= new Font("Ink Free", Font.BOLD,30);
-    
+
+    //Definimos la tipografía de nuestra aplicación
+    Font myFont= new Font("Impact", Font.BOLD,30);
+
+
+    //Definimos las variables que almacenaran los números y los resultados
     double num1=0,num2=0,resultado=0;
     char simboloOperacion;
 
@@ -26,15 +32,21 @@ public class calculadora implements ActionListener{
     //Constructor
     calculadora(){
 
-        frame = new JFrame("Calculadora");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getContentPane().setBackground(Color.BLACK);
-        frame.setSize(420,550);
-        frame.setResizable(false);
-        frame.setLayout(null);
+        //Aqui estará almacenado todo la inicialización del programa, para que cuando se llame en el main se ejecute enseguida
 
+
+        //Creamos el frame de nuestra aplicación
+        frame = new JFrame("Calculadora");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//Le pasamos la instrucción que al cerrar la ventana cierra el progama también
+        frame.getContentPane().setBackground(Color.BLACK);//Cambiamos el fondo a negro
+        frame.setSize(420,550);//Definimos el tamaño
+        frame.setResizable(false);//Definimos si se puede redimensionar la ventana
+        frame.setLayout(null);//Como modificaremos el layout mas adelante, quitaremos el predeterminado
+
+
+        //Creamos nuestra input de texto
         textField = new JTextField();
-        textField.setBounds(50, 25, 300, 50);
+        textField.setBounds(50, 25, 300, 50);//Le pasamos el tamaño y la posición
         textField.setFont(myFont);
         textField.setEditable(false);//Con esto nos aseguramos que el espacio no pueda ser editado por el teclado, ya que no permitimos que el campo pueda ser editado
 
@@ -64,8 +76,8 @@ public class calculadora implements ActionListener{
         botonFunciones[8] = negButton;
 
         for(int i = 0; i<9; i++){
-                botonFunciones[i].addActionListener(this);
-                botonFunciones[i].setFont(myFont);
+                botonFunciones[i].addActionListener(this); //Le añadimos a cada boton un action Listener para poder "escuchar" cuando el usuario interactua con el botón
+                botonFunciones[i].setFont(myFont);//Añadimos nuestra fuente al botón
                 botonFunciones[i].setFocusable(false);
                 botonFunciones[i].setBackground(Color.ORANGE);
                 botonFunciones[i].setOpaque(true);
@@ -85,6 +97,7 @@ public class calculadora implements ActionListener{
             numeroBotones[i].setBorderPainted(false);
         }
 
+        //Como estos botones no van dentro del Jpanel se tiene que ajustar sus posiciones con respecto al Jframe
         negButton.setBounds(50,430,100,50);
         delButton.setBounds(150,430,100,50);
         clrButton.setBounds(250,430,100,50);
@@ -137,6 +150,7 @@ public class calculadora implements ActionListener{
 
     public static void main (String[] args ){
 
+        //Instanciamos nuestra clase calculadora y esta ejecutará el programa
         calculadora calc = new calculadora();
     }
 
@@ -146,16 +160,21 @@ public class calculadora implements ActionListener{
     public void actionPerformed(ActionEvent e) {
        
 
+        //Es un loop que recorre el arreglo de numeroBotones, y va comparando si el botón o el componente que al cual se le hizo click es igual al boton
+        // de algun número dentro del arreglo, de ser asi modifica el textField con el número presionado
         for(int i =0; i<10; i++){
             if(e.getSource() == numeroBotones[i]){
                 textField.setText(textField.getText().concat(String.valueOf(i)));
             }
         }
 
+
+        //Verifica si el botón clickado es igual al boton de decimal
         if(e.getSource() == decButton){
             textField.setText(textField.getText().concat("."));
         }
 
+        //Verifica si el botón clickado es igual al boton de suma
         if(e.getSource() == addButton){
            num1= Double.parseDouble(textField.getText());
            simboloOperacion = '+';
@@ -163,6 +182,7 @@ public class calculadora implements ActionListener{
 
         }
 
+        //Verifica si el botón clickado es igual al boton de resta
         if(e.getSource() == subButton){
             num1= Double.parseDouble(textField.getText());
             simboloOperacion = '-';
@@ -170,6 +190,8 @@ public class calculadora implements ActionListener{
  
          }
 
+
+        //Verifica si el botón clickado es igual al boton de multiplicación
          if(e.getSource() == mulButton){
             num1= Double.parseDouble(textField.getText());
             simboloOperacion = 'x';
@@ -177,6 +199,7 @@ public class calculadora implements ActionListener{
  
          }
 
+        //Verifica si el botón clickado es igual al boton de división
          if(e.getSource() == divButton){
             num1= Double.parseDouble(textField.getText());
             simboloOperacion = '/';
@@ -184,6 +207,8 @@ public class calculadora implements ActionListener{
  
          }
 
+
+         //Verifica si el botón clickado es igual al boton de igual, de ser así se obtiene el último número ingresado por el usuario y con un switchcase verificamos que operación es la que se va a realizar
          if(e.getSource() == eqButton){
             num2 = Double.parseDouble(textField.getText());
 
@@ -203,20 +228,25 @@ public class calculadora implements ActionListener{
                     break;
             }
             
+            //Imprime el resultado
             textField.setText(String.valueOf(resultado));
-            num1=resultado;
+            num1=resultado; //Se define como numero 1 el resultado en caso de que el usuario quiere seguir operando con el resultado de la operación anterior
 
          }
 
+         //Limpia el textfield definiendolo otra vez como un espacio vacio
          if(e.getSource() == clrButton){
             textField.setText("");
  
          }
 
+         //Verifica si el botón clickado es igual al boton de borrar
          if(e.getSource() == delButton){
-            String string = textField.getText();
-            textField.setText("");
+            String string = textField.getText(); //Guardamos el texto temporalmente
+            textField.setText("");//Borramos el texto que hay en el textfield
 
+
+            //Este loop imprimira y concatenara cada elemento del texto exceptuando el último carácter dando la impresión de haber "eliminado" el ultimo caracter
             for(int i= 0; i<string.length()-1;i++){
                 textField.setText(textField.getText()+string.charAt(i));
             }
